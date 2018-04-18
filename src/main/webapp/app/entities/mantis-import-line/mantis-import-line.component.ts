609f8cf08039217762c342a7332fe06210443186
Cédric <cedric@zelixgroup.com>
2018-04-18 08:48:12 +0400

    finalize filter, status (in progress)

--- src/main/webapp/app/entities/mantis-import-line/mantis-import-line.component.ts
+++ src/main/webapp/app/entities/mantis-import-line/mantis-import-line.component.ts
@@ -4,7 +4,7 @@ import { ActivatedRoute, Router } from '@angular/router';
 import { Subscription } from 'rxjs/Subscription';
 import { JhiEventManager, JhiParseLinks, JhiAlertService } from 'ng-jhipster';
 
-import { FormArray, FormGroup, FormControl, FormBuilder } from '@angular/forms';
+import { FormArray, FormGroup, FormBuilder } from '@angular/forms';
 
 import { MantisImportLine } from './mantis-import-line.model';
 import { MantisImport, MantisImportService } from '../mantis-import';
@@ -16,7 +16,7 @@ import { ITEMS_PER_PAGE, Principal } from '../../shared';
     templateUrl: './mantis-import-line.component.html'
 })
 export class MantisImportLineComponent implements OnInit, OnDestroy {
-  
+
     currentAccount: any;
     mantisImportLines: MantisImportLine[];
     error: any;
@@ -32,8 +32,8 @@ export class MantisImportLineComponent implements OnInit, OnDestroy {
     previousPage: any;
     reverse: any;
     criteria: any;
-    
-    mantisimports: MantisImport[];
+
+    mantisImports: MantisImport[];
 
     rowModels: any[];
     filterForm: FormGroup;
@@ -52,7 +52,6 @@ export class MantisImportLineComponent implements OnInit, OnDestroy {
         private formBuilder: FormBuilder,
         private mantisImportService: MantisImportService,
     ) {
-    
 
         this.itemsPerPage = ITEMS_PER_PAGE;
         this.routeData = this.activatedRoute.data.subscribe((data) => {
@@ -61,70 +60,61 @@ export class MantisImportLineComponent implements OnInit, OnDestroy {
             this.reverse = data.pagingParams.ascending;
             this.predicate = data.pagingParams.predicate;
         });
-        this.criteria = {
-            mantisNumber: null,
-            areSet() {
-                return this.mantisNumber != null ;
-            },
-            clear() {
-                this.mantisNumber = null;
-            }
-        }
+
         this.filterFields = this.getFilterFields();
         this.filterComparators = this.getFilterComparators(null);
 
         this.rowModels = [{}];
         this.filterForm = this.formBuilder.group({
+          filterMantisImport: [1],
           filterRows: this.formBuilder.array([this.initFilterRows()])
         });
-        const control = <FormArray>this.filterForm.controls['filterRows'];
-        const group = <FormGroup>control.controls[0];
-        group.controls['concernedField'].setValue('mantisNumber', {onlySelf: true});
-        //group.controls['criteriaValue'].setValue('cool', {onlySelf: true});
     }
 
-  initFilterRows(){
-    this.rowModels.push({comparators:[]});
-    return this.formBuilder.group({
-        concernedField: {label:'mantisNumber', value:'mantisNumber', category:'text'},
-        comparator: {key:'contains', value:'contains', label:'Contains'},
-        criteriaValue: '',
-    });
-  }
-  addCriteria(){
-    const control = <FormArray>this.filterForm.controls['filterRows'];
-    control.push(this.initFilterRows());
-  }
+    initFilterRows() {
+      this.rowModels.push({comparators: []});
+      return this.formBuilder.group({
+          concernedField: {label: 'mantisNumber', value: 'mantisNumber', category: 'text'},
+          comparator: {key: 'contains', value: 'contains', label: 'Contains'},
+          criteriaValue: '',
+      });
+    }
 
-  removeCriteria(index){
-    const control = <FormArray>this.filterForm.controls['filterRows'];
-    control.removeAt(index);
-    this.rowModels.splice(index, 1);
-  }
+    addCriteria() {
+      const control = <FormArray>this.filterForm.controls['filterRows'];
+      control.push(this.initFilterRows());
+    }
 
-  fieldChanged(event, index){
-    console.log(event);
-    this.rowModels[index].comparators = this.getFilterComparators(event);
-  }
+    removeCriteria(index) {
+      const control = <FormArray>this.filterForm.controls['filterRows'];
+      control.removeAt(index);
+      this.rowModels.splice(index, 1);
+    }
 
-  comparatorChanged(event){
-    console.log(event);
-  }
+    fieldChanged(event, index) {
+      this.rowModels[index].comparators = this.getFilterComparators(event);
+    }
+
+    comparatorChanged(event) {
+    }
 
     loadAll() {
         const criteria = [];
 
-        this.filterForm.value.filterRows.forEach(function (value) {
+        if (this.filterForm.value.filterMantisImport !== '') {
+          criteria.push({key: 'mantisImportId.equals', value: this.filterForm.value.filterMantisImport});
+        }
+
+        this.filterForm.value.filterRows.forEach(function(value) {
           console.log(value);
-          if(value.concenedField !== null && value.concernedField !== '' &&
-              value.comparator !== null && value.comparator !== '' && 
-              value.criteriaValue !== null && value.criteriaValue !== ''){
-              if(value.comparator.key === 'in'){
-              	criteria.push({key: value.concernedField.value + '.' + value.comparator.key, value: value.criteriaValue.split('|')});
-              }else{
-              	criteria.push({key: value.concernedField.value + '.' + value.comparator.key, value: value.criteriaValue});
+          if (value.concenedField !== null && value.concernedField !== '' &&
+              value.comparator !== null && value.comparator !== '' &&
+              value.criteriaValue !== null && value.criteriaValue !== '') {
+              if (value.comparator.key === 'in') {
+                  criteria.push({key: value.concernedField.value + '.' + value.comparator.key, value: value.criteriaValue.split('|')});
+              }else {
+                  criteria.push({key: value.concernedField.value + '.' + value.comparator.key, value: value.criteriaValue});
               }
-            
           }
         });
 
@@ -165,26 +155,19 @@ export class MantisImportLineComponent implements OnInit, OnDestroy {
     }
 
     ngOnInit() {
-    	setTimeout(() => {
-	        this.mantisImportService.query().subscribe((res: HttpResponse<MantisImport[]>) => { 
-	            	this.mantisimports = res.body; 
-	            	console.log(this.mantisimports);
-	            }, 
-	            (res: HttpErrorResponse) => this.onError(res.message)
-	        );
-        }, 0);
-       
-        
+      this.mantisImportService.query().subscribe((res: HttpResponse<MantisImport[]>) => {
+                this.mantisImports = res.body;
+              },
+              (res: HttpErrorResponse) => this.onError(res.message)
+          );
+
         this.loadAll();
-        
-        
-        
+
         this.principal.identity().then((account) => {
             this.currentAccount = account;
         });
-        
+
         this.registerChangeInMantisImportLines();
-		
     }
 
     ngOnDestroy() {
@@ -223,56 +206,56 @@ export class MantisImportLineComponent implements OnInit, OnDestroy {
         this.jhiAlertService.error(error.message, null, null);
     }
 
-  getFilterComparators(field: any){
-    let comparators = [];
-    if(field !== null){
-      if(field.category == "text"){
+    getFilterComparators(field: any) {
+      let comparators = [];
+      if (field !== null) {
+        if (field.category === 'text') {
+            comparators = [
+            {key: 'contains', value: 'contains', label: 'Contains'},
+              {key: 'equals', value: 'equals', label: 'Equals (=)'},
+              {key: 'in', value: 'in', label: 'In'},
+            ];
+        }else if (field.category === 'number' || field.category === 'date') {
           comparators = [
-          {key:'contains', value:'contains', label:'Contains'},
-            {key:'equals', value:'equals', label:'Equals (=)'},
-            {key:'in', value:'in', label:'In'},
+              {key: 'equals', value: 'equals', label: 'Equals (=)'},
+              {key: 'greaterThan', value: 'greaterThan', label: 'Greater than (>)'},
+              {key: 'lessThan', value: 'lessThan', label: 'Less than (<)'},
+              {key: 'greaterOrEqualThan', value: 'greaterOrEqualThan', label: 'Greater than or equals to (>=)'},
+              {key: 'lessOrEqualThan', value: 'lessOrEqualThan', label: 'Less than or equals to (<=)'},
           ];
-      }else if(field.category == "number" || field.category == "date"){
-        comparators = [
-            {key:'equals', value:'equals', label:'Equals (=)'},
-            {key:'greaterThan', value:'greaterThan', label:'Greater than (>)'},
-            {key:'lessThan', value:'lessThan', label:'Less than (<)'},
-            {key:'greaterOrEqualThan', value:'greaterOrEqualThan', label:'Greater than or equals to (>=)'},
-            {key:'lessOrEqualThan', value:'lessOrEqualThan', label:'Less than or equals to (<=)'},
-        ];
+        }
       }
+
+      return comparators;
     }
 
-    return comparators;
-  }
-
-  getFilterFields(){
-    let fields = [
-        {label:'mantisNumber', value:'mantisNumber', category:'text'},
-        {label:'validationStatus', value:'validationStatus', category:'text'},
-        {label:'project', value:'project', category:'text'},
-        {label:'updateDate', value:'updateDate', category:'date'},
-        {label:'category', value:'category', category:'text'},
-        {label:'gravity', value:'gravity', category:'text'},
-        {label:'augeoReference', value:'augeoReference', category:'text'},
-        {label:'technicalReference', value:'technicalReference', category:'text'},
-        {label:'description', value:'description', category:'text'},
-        {label:'submissionDate', value:'submissionDate', category:'date'},
-        {label:'desiredCommitmentDate', value:'desiredCommitmentDate', category:'date'},
-        {label:'estimatedChargeCACF', value:'estimatedChargeCACF', category:'number'},
-        {label:'commitmentDateCDS', value:'commitmentDateCDS', category:'date'},
-        {label:'estimatedChargeCDS', value:'estimatedChargeCDS', category:'number'},
-        {label:'estimatedDSTDelivreryDate', value:'estimatedDSTDelivreryDate', category:'date'},
-        {label:'recipeDate', value:'recipeDate', category:'date'},
-        {label:'productionDate', value:'productionDate', category:'date'},
-        {label:'estimatedChargeCDS', value:'estimatedChargeCDS', category:'number'},
-        {label:'devStandardsComplianceScore', value:'devStandardsComplianceScore', category:'text'},
-        {label:'devStandardsComplianceScoreComment', value:'devStandardsComplianceScore', category:'text'},
-        {label:'expressedNeedsComplianceScore', value:'expressedNeedsComplianceScore', category:'text'},
-        {label:'expressedNeedsComplianceScoreComment', value:'expressedNeedsComplianceScoreComment', category:'text'},
-        {label:'overallDeadlineRespectScore', value:'overallDeadlineRespectScore', category:'text'},
-        {label:'overallDeadlineRespectScoreComment', value:'overallDeadlineRespectScoreComment', category:'text'},
-    ];
-    return fields;
-  }
+    getFilterFields() {
+      const fields = [
+          {label: 'mantisNumber', value: 'mantisNumber', category: 'text'},
+          {label: 'validationStatus', value: 'validationStatus', category: 'text'},
+          {label: 'project', value: 'project', category: 'text'},
+          {label: 'updateDate', value: 'updateDate', category: 'date'},
+          {label: 'category', value: 'category', category: 'text'},
+          {label: 'gravity', value: 'gravity', category: 'text'},
+          {label: 'augeoReference', value: 'augeoReference', category: 'text'},
+          {label: 'technicalReference', value: 'technicalReference', category: 'text'},
+          {label: 'description', value: 'description', category: 'text'},
+          {label: 'submissionDate', value: 'submissionDate', category: 'date'},
+          {label: 'desiredCommitmentDate', value: 'desiredCommitmentDate', category: 'date'},
+          {label: 'estimatedChargeCACF', value: 'estimatedChargeCACF', category: 'number'},
+          {label: 'commitmentDateCDS', value: 'commitmentDateCDS', category: 'date'},
+          {label: 'estimatedChargeCDS', value: 'estimatedChargeCDS', category: 'number'},
+          {label: 'estimatedDSTDelivreryDate', value: 'estimatedDSTDelivreryDate', category: 'date'},
+          {label: 'recipeDate', value: 'recipeDate', category: 'date'},
+          {label: 'productionDate', value: 'productionDate', category: 'date'},
+          {label: 'estimatedChargeCDS', value: 'estimatedChargeCDS', category: 'number'},
+          {label: 'devStandardsComplianceScore', value: 'devStandardsComplianceScore', category: 'text'},
+          {label: 'devStandardsComplianceScoreComment', value: 'devStandardsComplianceScore', category: 'text'},
+          {label: 'expressedNeedsComplianceScore', value: 'expressedNeedsComplianceScore', category: 'text'},
+          {label: 'expressedNeedsComplianceScoreComment', value: 'expressedNeedsComplianceScoreComment', category: 'text'},
+          {label: 'overallDeadlineRespectScore', value: 'overallDeadlineRespectScore', category: 'text'},
+          {label: 'overallDeadlineRespectScoreComment', value: 'overallDeadlineRespectScoreComment', category: 'text'},
+      ];
+      return fields;
+    }
 }
